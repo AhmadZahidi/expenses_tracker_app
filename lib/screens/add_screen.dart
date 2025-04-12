aae8b6613919f50572ddad8989089dcb6289abd2
Zahidi <ahmadzahidi120@gmail.com>
2025-04-12 22:05:05 +0800

    working on add page

--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -3,11 +3,105 @@ import 'package:expenses_tracker_app/reusable%20widget/background_screen.dart';
 import 'package:expenses_tracker_app/reusable%20widget/crudbar.dart';
 import 'package:flutter/material.dart';
 
-class AddScreen extends StatelessWidget{
+class AddScreen extends StatefulWidget {
   const AddScreen({super.key});
-  
+
+  @override
+  State<StatefulWidget> createState() {
+    return _AddScreenState();
+  }
+}
+
+class _AddScreenState extends State<AddScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _categoryController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _quantityController = TextEditingController();
+
+  String? _selectedCategory;
+  final List<String> _categories = [
+    'Food',
+    'Transport',
+    'Rent',
+    'Hobby',
+    'Health',
+    'Other',
+  ];
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _categoryController.dispose();
+    _priceController.dispose();
+    _quantityController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BackgroundScreen(appBar:Crudbar('Add Expenses'),backgroundColor:mainBackGround, screen:Column());
+    return BackgroundScreen(
+      appBar: Crudbar('Add Expenses'),
+      backgroundColor: mainBackGround,
+      screen: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Name
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color.fromARGB(100, 0, 0, 0)),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 14,
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+
+            //Category
+            DropdownButtonFormField(
+              value: _selectedCategory,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white ,
+                labelText: 'Category',
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 16,
+                ),
+              ),
+              items:
+                  _categories.map((category) {
+                    return DropdownMenuItem(
+                      value: category,
+                      child: Text(category),
+                    );
+                  }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _selectedCategory = value;
+                });
+              },
+              validator:
+                  (value) => value == null ? "Please select a category" : null,
+            ),
+            SizedBox(height: 30,),
+
+            //Price
+            
+          ],
+        ),
+      ),
+    );
   }
 }
